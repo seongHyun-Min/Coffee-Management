74b74cb82452988321c35dd1ddea7f7290a03347
seongHyun-Min <steve6133@naver.com>
2023-07-10 00:24:21 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/example/coffeemanagement/CoffeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coffeemanagement/CoffeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
