538f12c0a61e123ad965989b5ddaf54f84ef33af
seongHyun-Min <steve6133@naver.com>
2023-07-11 15:23:34 +0900

    refactor : product 필드명 수정 updateAt -> updatedAt

--- src/main/java/com/example/coffeemanagement/model/Product.java
+++ src/main/java/com/example/coffeemanagement/model/Product.java
@@ -21,7 +21,7 @@ public class Product {
     private String description;
     private final LocalDateTime createdAt;
 
-    private LocalDateTime updateAt;
+    private LocalDateTime updatedAt;
 
     public Product(UUID productId, String productName, Category category, long price) {
         this.productId = productId;
@@ -29,29 +29,29 @@ public class Product {
         this.category = category;
         this.price = price;
         this.createdAt = LocalDateTime.now();
-        this.updateAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
     public void setProductName(String productName) {
         this.productName = productName;
-        this.updateAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
     public void setCategory(Category category) {
         this.category = category;
-        this.updateAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
 
     }
 
     public void setPrice(long price) {
         this.price = price;
-        this.updateAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
 
     }
 
     public void setDescription(String description) {
         this.description = description;
-        this.updateAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
 
     }
 }
